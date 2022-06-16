851681b657b866560387c1f4cd088c3761961bac
gem-vivekanandtiwari <vivekanand.tiwari@geminisolutions.in>
2022-06-16 12:13:26 +0530

    Completed Set up for TickerTape

---
+++ src/main/java/com/qa/gemini/ticker/stepdefinition/TickerTape.java
@@ -0,0 +1,13 @@
+package com.qa.gemini.ticker.stepdefinition;
+
+import io.cucumber.java.en.Given;
+
+public class TickerTape {
+
+
+    @Given("Search SBI in the search box")
+    public void search_sbi_in_the_search_box() {
+       System.out.println("Inside Ticker Tape");
+    }
+
+}
---
+++ src/main/resources/features/TickerTapeHomepage.feature
@@ -0,0 +1,5 @@
+Feature: TickerTapeHomepage
+
+
+  Scenario:Launch Tickertape and search for SBI
+    Given Search SBI in the search box
